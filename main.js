f824195aecd40ae284e0bbd1d84625ad2486f2e8
25LP7649 25LP7649 <25LP7649@admins-iMac.local>
2025-10-16 11:57:55 +0800

    as

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ import ora from "ora";
 import pokemon from "pokemon";
 import { generate, count } from "random-words";
 import { say } from "cowsay";
+import { faker } from "@faker-js/faker";
+import { fs } from "fs";
+import { log } from "console";
 
 // figlet("Hello World!!", function (err, data) {
 //   if (err) {
@@ -26,4 +29,36 @@ import { say } from "cowsay";
 
 // console.log(generate(5));
 
-console.log(say({ text: "grazing in the browser" }));
+// console.log(say({ text: "grazing in the browser" }));
+
+// export function createRandomUser() {
+//   return {
+//     userId: faker.string.uuid(),
+//     username: faker.internet.username(),
+//     email: faker.internet.email(),
+//     avatar: faker.image.avatar(),
+//     password: faker.internet.password(),
+//     birthdate: faker.date.birthdate(),
+//     registeredAt: faker.date.past(),
+//   };
+// }
+
+// export const users = faker.helpers.multiple(createRandomUser, {
+//   count: 5,
+// });
+// console.log(users);
+
+fs.readFile("data.txt", "utf8", (err, data) => {
+  if (err) {
+    console.log("error");
+  }
+  const search = "async";
+  const index = data.indexOf(search);
+
+  if (index != -1) {
+    `console.log(this text exist at ${index}`;
+  } else {
+    console.log(`you
+    `);
+  }
+});
